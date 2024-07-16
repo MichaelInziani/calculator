7bc142c24947b996429a39d25b77e02feb907053
Michael Inziani <mikeinziani004@gmail.com>
2024-07-16 11:35:37 +0300

    Python Practise

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def multiply(n1, n2):
 
 #Division
 def divide(n1, n2):
+    if n2 == 0:
+        return 0
     return n1/n2
 
 operator = {
@@ -21,6 +23,14 @@ operator = {
     "*": multiply,
     "/": divide
 }
+def takeOperation():
+    operator_symbol = input("Select an operator from the ones above: ")
+
+    if operator_symbol not in operator.keys():
+        print("Invalid input. Please select an operator from the ones given.")
+        return takeOperation()
+    else:
+        return operator_symbol
 
 def calculator():
     print(logo)
@@ -30,11 +40,12 @@ def calculator():
     while should_continue:
         for symbol in operator:
             print(symbol)
-        operator_symbol = input("Select an operator from the ones above: ")
+        #operator_symbol = input("Select an operator from the ones above: ")
+        operation = takeOperation()
         second_number = float(input("Enter the next number: "))
-        calculation_function = operator[operator_symbol]
+        calculation_function = operator[operation]
         answer = calculation_function(first_number, second_number)
-        print(f"{first_number} {operator_symbol} {second_number} = {answer} ")
+        print(f"{first_number} {operation} {second_number} = {answer} ")
 
         next_number = input(f"Enter 'y' to continue calculating with {answer} or 'n' to start a new calculation: ")
         if next_number == "y":
